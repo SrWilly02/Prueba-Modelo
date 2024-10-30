9503642b39b24855c03e4578e1872070ca75b126
WillySotelo <125620006+SrWilly02@users.noreply.github.com>
2024-10-30 12:37:15 -0500

    Add files via upload

---
+++ Detect.py
@@ -0,0 +1,28 @@
+# Importar librerías
+import torch
+import cv2
+import numpy as np
+import os
+import pathlib
+
+# Importante para que Windows sea capaz de emplear PosixPath
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# Cargar el modelo
+model = torch.hub.load('ultralytics/yolov5', 'custom', path= r'C:\Users\USER\Desktop\Prueba Modelo\model\best.pt', force_reload=True)
+
+# Leer la imagen
+image_path = 'captura/captura_chrome2.png'
+image = cv2.imread(image_path)
+
+# Realizar detecciones
+results = model(image)
+
+# Convertir los resultados en formato de imagen
+results_img = np.squeeze(results.render())
+
+# Mostrar la imagen con detecciones
+cv2.imshow('Detecciones YOLOv5', results_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
