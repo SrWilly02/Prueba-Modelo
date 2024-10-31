c8bace98269bbc0d635d5c56f39b0b6873fc935b
WillySotelo <125620006+SrWilly02@users.noreply.github.com>
2024-10-31 18:47:02 -0500

    Update Detect.py

--- Detect.py
+++ Detect.py
@@ -10,14 +10,32 @@ temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
 # Cargar el modelo
-model = torch.hub.load('ultralytics/yolov5', 'custom', path= 'model/best.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'model/best.pt', force_reload = True)
 
 # Leer la imagen
-image_path = 'captura/captura_chrome2.png'
+image_path = 'captura/captura_chrome3.jpg'
 image = cv2.imread(image_path)
 
 # Realizar detecciones
 results = model(image)
+
+# Extraer etiquetas y probabilidades
+detections = results.pandas().xyxy[0]  # Obtiene el DataFrame con las detecciones
+total_detections = len(detections)
+
+# Verificar si hay detecciones
+if total_detections > 0:
+    phishing_count = detections[detections['name'] == 'Phishing'].shape[0]
+    no_phishing_count = detections[detections['name'] == 'No Phishing'].shape[0]
+
+    # Calcular el porcentaje de cada etiqueta
+    phishing_percentage = (phishing_count / total_detections) * 100
+    no_phishing_percentage = (no_phishing_count / total_detections) * 100
+
+    print(f"Porcentaje de Phishing: {phishing_percentage:.2f}%")
+    print(f"Porcentaje de No Phishing: {no_phishing_percentage:.2f}%")
+else:
+    print("No se detectaron elementos en la imagen.")
 
 # Convertir los resultados en formato de imagen
 results_img = np.squeeze(results.render())
